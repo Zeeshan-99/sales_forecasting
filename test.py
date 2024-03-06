2ed15dedaa63c7d1067f75474bd0e1b23ac7f2a7
ZEESHAN HALEEM <88664935+Zeeshan-99@users.noreply.github.com>
2024-03-06 14:05:01 +0530

    Added pycaret forecasting code runing with MLFlow

--- test.py
+++ test.py
@@ -4,12 +4,18 @@ Created on Monday Nov 27 2023
 @author: Zeeshan
 """
 
+from flask import Flask,render_template,redirect,request
+import pandas as pd
+import seaborn as sns
+import numpy as np
 from flask import Flask
+
 app = Flask(__name__)
 
+app.config["IMAGE_UPLOADS"] = "static/img/"
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template("test.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
